9ed3385e59f5a590ee86b75686959c8d7bf779f7
cmsanchezg <sanchez.carlomg@gmail.com>
2023-06-19 23:41:48 -0700

    added background, style to the hero section. added the resume icon

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,27 +2,21 @@ import './Hero.scss';
 
 import linkedin from '../../assets/icons/linkedin.svg';
 import gitHub from '../../assets/icons/github.svg';
-// import leetCode from '../../assets/icons/leetcode.svg';
 import email from '../../assets/icons/email.svg';
-
+import resume from '../../assets/icons/resume.png';
 
 function Hero() {
     return (
         <article className='hero'>
-            <section className='hero__name'>
-                Hello! My name is Carlos Sanchez!
-            </section>
-            <section className='hero__location'>
-                I am a Web Developer based in Vancouver, B.C. 
-            </section>      
+            <p className='hero__name'>Hello! My name is <span>Carlos Sanchez!</span></p>
+            <p className='hero__location'>I am a <span>Web Developer</span> based in <span>Vancouver, B.C.</span></p>       
             <section className='hero__social'>
                 <img className='hero__social__icon' src={linkedin} alt="LinkedIn Logo"/>
                 <img className='hero__social__icon' src={gitHub} alt="Git Hub Logo"/>
                 <img className='hero__social__icon' src={email} alt="Email Logo"/>
+                <img className='hero__social__icon' src={resume} alt="Resume Logo"/>
             </section>    
-            <section>
-                scroll down to get to know me better!
-            </section>
+            <p className='hero__scroll__down'>Scroll down to get to know me better!</p>
         </article>
     );
 }
