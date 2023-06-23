2badfbd900bdfdc9794aaddfecf4774cb11f5a99
cmsanchezg <sanchez.carlomg@gmail.com>
2023-06-23 15:54:49 -0700

    added cox shadow effect on the mobile dropdown menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import './Header.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import headShot from '../../assets/images/carlos-sanchez-image.jpeg';
 
@@ -14,27 +14,42 @@ function Header() {
             </section>
             <section className='header__nav'>
                 <div className='header__hamburger__menu'>
-                    <input type="checkbox"  id='checkbox' className='header__checkbox  header__visuallyHidden'/> 
+                    <input type="checkbox"  id='checkbox' className='header__checkbox  header__hidden'/> 
                     <label htmlFor="checkbox">
                         <div className='header__hamburger'>
                             <span className='header__bar header__bar1'></span>
                             <span className='header__bar header__bar2'></span>
-                            <span className='header__bar header__bar3'></span>
-                            <span className='header__bar header__bar4'></span>    
+                            <span className='header__bar header__bar3'></span>    
                         </div> 
                     </label>
                     <ul className='header__nav__links'>
-                        <Link className='header__link'>Home</Link>
-                        <Link className='header__link'>About Me</Link>
-                        <Link className='header__link'>Projects</Link>
-                        <Link className='header__link'>Contact</Link>
+                        <li className='header__nav__item'>
+                            <NavLink className='header__nav__link'>Home</NavLink>
+                        </li>
+                        <li className='header__nav__item'>
+                            <NavLink className='header__nav__link'>About Me</NavLink>
+                        </li>
+                        <li className='header__nav__item'>
+                            <NavLink className='header__nav__link'>Projects</NavLink>
+                        </li>
+                        <li className='header__nav__item'>
+                            <NavLink className='header__nav__link'>Contact</NavLink>
+                        </li>
                     </ul>
                 </div>
                 <ul className='header__nav__links header__nav__links--tablet'>
-                    <Link className='header__link header__link--tablet'>Home</Link>
-                    <Link className='header__link header__link--tablet'>About Me</Link>
-                    <Link className='header__link header__link--tablet'>Projects</Link>
-                    <Link className='header__link header__link--tablet'>Contact</Link>
+                    <li className='header__nav__item header__nav__item--tablet'>
+                        <NavLink className='header__nav__link header__nav__link--tablet'>Home</NavLink>
+                    </li>
+                    <li className='header__nav__item header__nav__item--tablet'>
+                        <NavLink className='header__nav__link header__nav__link--tablet'>About Me</NavLink>
+                    </li>
+                    <li className='header__nav__item header__nav__item--tablet'>
+                        <NavLink className='header__nav__link header__nav__link--tablet'>Projects</NavLink>
+                    </li>
+                    <li className='header__nav__item header__nav__item--tablet'>
+                        <NavLink className='header__nav__link header__nav__link--tablet'>Contact</NavLink>
+                    </li>
                 </ul> 
             </section>
         </article>
