0ad5664c1ca9e499ae9e6b3efb2522453173017e
cmsanchezg <sanchez.carlomg@gmail.com>
2023-06-23 23:00:49 -0700

    edited dropdown nenu for mobile (drops from under the header and vanishes from the same location with a box shadow effect)

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,25 +1,28 @@
 import { Link } from 'react-router-dom';
 import './AboutMe.scss';
+import education from '../../assets/icons/education.svg';
+import experience from '../../assets/icons/experience.svg';
+import projects from '../../assets/icons/projects.svg';
 
 function AboutMe() {
 
     return (
         <>
        <article className='aboutMe'>
-            <img src="" alt="" />
+            <img src="" alt="Carlos Sanchez" />
             <div>
                 <div>
-                    <img src="" alt="Education Emoji" />
+                    <img className='aboutMe__icon' src={education} alt="Education Emoji" />
                     <p>BrainStation</p>
                     <p>(Diploma)</p>
                 </div>
                 <div>
-                    <img src="" alt="Experience Emoji" />
+                    <img className='aboutMe__icon' src={experience} alt="Experience Emoji" />
                     <p>Experience</p>
                     <p>(6+ months)</p>
                 </div>
                 <div>
-                    <img src="" alt="Laptop Emoji" />
+                    <img className='aboutMe__icon' src={projects} alt="Laptop Emoji" />
                     <p>Projects</p>
                     <p>(6+ projects)</p>
                 </div>
