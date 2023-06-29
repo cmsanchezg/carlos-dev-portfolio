c72538554f8853f724cd9eb322226c1894a8980d
cmsanchezg <sanchez.carlomg@gmail.com>
2023-06-29 00:21:35 -0700

    create contact section (jsx and scss), create form and added style (pending to add functionality)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,22 @@ function Contact() {
     return (
         <>
        <article className='contact'>
-        
+            <h1 className='contact__header'>Lets get in touch!</h1>
+            <form className='contact__forms' action="">
+                <div className='contact__form'>
+                    <label className='contact__form__label' htmlFor="name">Name:</label>
+                    <input className='contact__form__input' type="text" />
+                </div>
+                <div className='contact__form'>
+                    <label className='contact__form__label' htmlFor="">Email:</label>
+                    <input className='contact__form__input' type="text" />
+                </div>
+                <div className='contact__form'>
+                    <label className='contact__form__label' htmlFor="">Message:</label>
+                    <textarea className='contact__form__textarea' name="" id="" rows="10"></textarea>
+                </div>
+                <button className='contact__form__button'>send</button>
+            </form>
         </article>
         </>
     );
