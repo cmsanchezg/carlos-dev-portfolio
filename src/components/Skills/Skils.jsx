a87336430d5161c282fd7768d316f2519907e906
cmsanchezg <sanchez.carlomg@gmail.com>
2023-07-01 16:12:15 -0700

    create wrap for frontend, backend and soft skill

--- src/components/Skills/Skils.jsx
+++ src/components/Skills/Skils.jsx
@@ -1,12 +1,31 @@
 import './Skills.scss';
 
-
 function Skills() {
 
     return (
         <>
        <article className='skills'>
            <h1 className='skills__header'>Skills</h1>
+           <div className='skills__container'>
+                <div className='skills__wrap'>
+                    <h2 className='skills__wrap__title'>Front-End</h2>
+                    <div className='skills__wrap__icons'>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div className='skills__wrap'>
+                    <h2 className='skills__wrap__title'>Back-End</h2>
+                    <div className='skills__wrap__icons'>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div className='skills__wrap'>
+                    <h2 className='skills__title'>Soft-Skills</h2>
+                    <div className='skills__wrap__icons'>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+           </div>
         </article>
         </>
     );
