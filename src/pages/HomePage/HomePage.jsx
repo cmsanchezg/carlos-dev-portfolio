161a318843b7a1404b7fd979636b8f2af93fdbfd
cmsanchezg <sanchez.carlomg@gmail.com>
2023-11-10 23:03:35 -0800

    adjusted the height for the hero section (mobile and tablet)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import Hero from "../../components/Hero/Hero";
 import AboutMe from '../../components/AboutMe/AboutMe';
 import Projects from '../../components/Projects/Projects';
 import Contact from '../../components/Contact/Contact';
-import Skills from '../../components/Skills/Skils';
+import Skills from '../../components/Skills/Skills';
 
 function HomePage() {
     return (
